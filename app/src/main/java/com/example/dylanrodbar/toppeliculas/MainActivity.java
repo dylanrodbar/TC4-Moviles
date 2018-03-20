47425288daf0220186f198d873cb8492167d3ff7
dylanrodbar <dylanrodbar97@gmail.com>
2018-03-20 17:47:50 -0600

    Plantilla de colores

---
+++ app/src/main/java/com/example/dylanrodbar/toppeliculas/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.dylanrodbar.toppeliculas;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.XML;
+import org.w3c.dom.Document;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+}
+
